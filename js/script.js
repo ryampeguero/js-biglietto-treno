4ac80f5124048caeb03619f8e0dad444b22455b5
ryampeguero <ryampeguero@gmail.com>
2024-03-26 10:55:10 +0100

    feat: added new conditions

--- js/script.js
+++ js/script.js
@@ -5,17 +5,22 @@ let price;
 const kilometer = parseInt(prompt("Quanti chilometri vuoi percorrere?")); // number
 const age = parseInt(prompt("Quanti anni hai?")); // number
 
-//Esecuzione logica
-price = kilometer * 0.21; // number
-console.log(price);
-if (age > 0 && age < 18) {
-    price = price - (price * 0.20);
-    document.getElementById("age").innerHTML = "Minorenne:"
-} else if (age >= 65 && age < 110) {
-    price = price - (price * 0.40);  
-    document.getElementById("age").innerHTML = "Over 65:" 
+if (!isNaN(kilometer) && !isNaN(age) && age > 0 && kilometer > 0 && age < 110) {
+    
+    //Esecuzione logica
+    price = kilometer * 0.21; // number
+    console.log(price);
+    if (age < 18) {
+        price = price - (price * 0.20);
+        document.getElementById("age").innerHTML = "Minorenne:"
+    } else if (age >= 65) {
+        price = price - (price * 0.40);  
+        document.getElementById("age").innerHTML = "Over 65:" 
+    }
+
+    //Output
+
+    document.getElementById("biglietto").innerHTML = price.toFixed(2);
+} else {
+    alert ("Dati errati!!")
 }
-
-//Output
-
-document.getElementById("biglietto").innerHTML = price.toFixed(2);
